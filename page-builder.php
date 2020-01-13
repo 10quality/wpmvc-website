f36e3c3a3fdbd6af1da4e9d8e5cb5c3e8d755f70
Alejandro Mostajo <amostajo@gmail.com>
2020-01-12 23:26:14 -0600

    #5 Fixes Builder template.
    `.container` removed. Padding removed. HTML refactored.

--- page-builder.php
+++ page-builder.php
@@ -5,46 +5,16 @@
  * @author Ale Mostajo <info@10quality.com>
  * @package wpmvc-website
  * @license MIT
- * @version 1.0.11
+ * @version 1.1.0
  */
 ?>
 <?php get_header() ?>
-
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-    <div class="doc-wrapper">
-        <div class="container">
-
-            <div class="doc-body">
-                <div class="row">
-                    <div
-                        <?php if ( get_page_has_sidebar() ) : ?>
-                            class="col-md-9 col-sm-8"
-                        <?php else : ?>
-                            class="col-md-12"
-                        <?php endif ?>
-                    >
-                        <?php the_content() ?>
-                        <?php do_action( 'wpmvc_after_page' ) ?>
-                    </div>
-
-                    <?php if ( get_page_has_sidebar() ) : ?>
-                        <div class="col-md-3 col-sm-4">
-                            <div class="widgets-wrapper">
-                                <?php if ( is_active_sidebar( 'wpmvc-page-right' ) ) : ?>
-                                    <?php dynamic_sidebar( 'wpmvc-page-right' ) ?>
-                                <?php else : ?>
-                                    <?php theme()->view( 'misc.no-widgets' ) ?>
-                                <?php endif ?>
-                            </div>
-                        </div>
-                    <?php endif ?>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
+    <div class="doc-wrapper page-builder">
+        <div class="doc-body">
+            <?php the_content() ?>
+            <?php do_action( 'wpmvc_after_page' ) ?>
+        </div><!--doc-body-->
+    </div><!--doc-wrapper-->
     <?php endwhile; ?><?php endif; ?>
-
 <?php get_footer() ?>
